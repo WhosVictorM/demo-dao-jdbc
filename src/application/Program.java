1ce82543ef5034b1f2d76e6235316f81c4fa3c0d
Victor <victormagalhaespb@gmail.com>
2023-07-21 19:55:02 -0300

    Implementing findByDepartment()

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -14,13 +15,21 @@ public class Program {
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
-		System.out.println("=== TEST 1: Seller findById() ===");
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Seller sell = sellerDao.findById(3);
+		System.out.println("=== TEST 1: Seller findById() ===");
 		
+		Seller sell = sellerDao.findById(3);
 		System.out.println(sell);
+		
+		System.out.println("=== TEST 2: Seller findByDepartment() ===");
+		
+		Department department = new Department(2, null);
+		
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 		
 		
 		
