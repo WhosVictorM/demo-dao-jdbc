7b7dd8da3fa64503e3e023d97740a0a428b9b741
Victor <victormagalhaespb@gmail.com>
2023-07-21 19:33:41 -0300

    Implementing findById()

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,15 +14,15 @@ public class Program {
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
-		Department obj = new Department(1, "Books");
+	
 		
-		Seller sell = new Seller(1, "Alex Green", "Alex@gmail.com",sdf.parse("28/09/1981"),1390.00, obj);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println(obj);
+		Seller sell = sellerDao.findById(3);
 		
 		System.out.println(sell);
+		
 		
 		
 		
